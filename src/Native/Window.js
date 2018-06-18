7f7f9d885d895c453f893fd931d805067e2e21ba
Manne Busk <mannebusk@gmail.com>
2018-06-18 12:47:56 +0200

    Changed how width and height is fetched, to correctly account for scrollbar space

--- src/Native/Window.js
+++ src/Native/Window.js
@@ -3,8 +3,8 @@ var _elm_lang$window$Native_Window = function()
 
 var size = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)	{
 	callback(_elm_lang$core$Native_Scheduler.succeed({
-		width: window.innerWidth,
-		height: window.innerHeight
+		width: document.body.getBoundingClientRect().width,
+		height: document.body.getBoundingClientRect().height
 	}));
 });
 
